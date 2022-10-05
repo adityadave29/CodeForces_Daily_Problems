852a67b7e63c36905306625d9068bde33ff3d549
Aditya Dave <cool2912004@gmail.com>
2022-10-05 19:43:19 +0530

    initial commit

---
+++ temp.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define f first
+#define s second
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define vi vector<int>
+#define mod 1000000007
+#define w(t)  \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define rep(i, c, d) for (int(i) = c; (i) <= (d); (i)++)
+#define repr(i, c, d) for (int(i) = c; (i) >= (d); (i)--)
+#define endl '\n'
+#define sortt(v) sort(v.begin(), v.end())
+#define rsortt(v) sort(v.rbegin(), v.rend());
+#define c(x) cout << x << " "
+#define p(x) cout << x << endl
+#define all(v) v.begin(), v.end()
+
+void solve()
+{
+}
+
+int main()
+{
+    FIO;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll tomato = 1;
+    cin >> tomato;
+    while (tomato--)
+    {
+        solve();
+    }
+    return 0;
+}
